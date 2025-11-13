30cddf0f2206fe35903496d834ce525ff3907c18
Edisson López <ediloaz@gmail.com>
2025-11-12 21:17:59 -0600

    Add robots and sitemap generation for SEO optimization

---
+++ app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://ediloaz.com'
+  
+  // Fecha de última modificación
+  const lastModified = new Date()
+  
+  return [
+    {
+      url: baseUrl,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about-me`,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/cv`,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified,
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ]
+}
+
